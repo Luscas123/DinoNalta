2193a837f04a168b4d9d787ae8da0bfa99664c61
Luscas123 <102263815+Luscas123@users.noreply.github.com>
2023-03-06 20:35:23 -0300

    Add files via upload

---
+++ screens/eei.js
@@ -0,0 +1,150 @@
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  ImageBackground,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import axios from "axios";
+
+export default class Eei extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getlocation();
+  }
+
+  getlocation = () => {
+    axios
+      .get("https://api.wheretheiss.at/v1/satellites/25544")
+      .then((response) => {
+        this.setState({ location: response.data });
+      });
+  };
+
+  render() {
+    if (Object.keys(this.state.location).length === 0) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>Carregando</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <ImageBackground
+            source={require("../assets/bg.png")}
+            style={styles.backgroundImage}
+          >
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>Localização da EEI</Text>
+            </View>
+            <View style={styles.mapContainer}>
+              <MapView
+                style={styles.map}
+                region={{
+                  latitude: this.state.location.latitude,
+                  longitude: this.state.location.longitude,
+                  latitudeDelta: 100,
+                  longitudeDelta: 100,
+                }}
+              >
+                <Marker
+                  coordinate={{
+                    latitude: this.state.location.latitude,
+                    longitude: this.state.location.longitude,
+                  }}
+                >
+                  <Image
+                    source={require("../assets/iss_icon.png")}
+                    style={{ height: 50, width: 50 }}
+                  />
+                </Marker>
+              </MapView>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text style={styles.infoText}>
+                Latitude: {this.state.location.latitude}
+              </Text>
+              <Text style={styles.infoText}>
+                Longitude: {this.state.location.longitude}
+              </Text>
+              <Text style={styles.infoText}>
+                Altitude (KM): {this.state.location.altitude}
+              </Text>
+              <Text style={styles.infoText}>
+                Velocidade (KM/H): {this.state.location.velocity}
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "rgba(0,0,0,0.3)" },
+  title: {
+    flex: 0.1,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "rgba(0,0,100,0.8)",
+  },
+
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+
+  mapContainer: {
+    flex: 0.6,
+  },
+  map: {
+    width: "95%",
+    height: "100%",
+    alignSelf: "center",
+    minHeight: "100%",
+    minWidth: "95%",
+  },
+  infoContainer: {
+    backgroundColor: "rgba(0,0,100,0.8)",
+    marginTop: -10,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 30,
+  },
+  infoText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
